e16d1c73840e775d8b4b7faefc78c5cbb72f093c
Clayton Ramstedt <clayton.ramstedt@gmail.com>
2019-02-21 21:32:06 -0700

    can play audio file

--- speaker_test/speaker_test.ino
+++ speaker_test/speaker_test.ino
@@ -8,10 +8,10 @@ void setup() {
 
 void loop() {
   // put your main code here, to run repeatedly:
- for (int i = 0; i < 255; i++) {
-  dac_output_voltage(DAC_CHANNEL_1, i);
+// for (int i = 0; i < 255; i++) {
+  dac_output_voltage(DAC_CHANNEL_1, 100);
   delay(1);
   dac_output_voltage(DAC_CHANNEL_1, 0);
   delay(1);
- }
+// }
 } 
--- wav.py
+++ wav.py
@@ -2,6 +2,7 @@ import wave
 import serial
 from struct import pack,unpack 
 import serial.tools.list_ports
+import time
 
 def getUSBDevice():
     # prints available devices
@@ -11,18 +12,33 @@ def getUSBDevice():
         if "CP210" in x[device].description:
             return x[device].device
 
+def readbytes():
+    output = b''
+    while ser.in_waiting:
+        output += ser.read(size=ser.in_waiting)
+        time.sleep(.001)
+    return output
+
 wav = wave.open("sorry_dave.wav", mode="rb")
-
-frame = wav.readframes(1)
-print("Sending out: ", frame)
 com = getUSBDevice()
-
+framerate = wav.getframerate()
+print("framerate: ", framerate)
 ser = serial.Serial(port=com, baudrate=115200)
-# ser.write(b'bob\n')
-# ser.write(pack("<cc", frame, b'\n'))
-ser.write(frame)
+# time.sleep(.1)
 
+# print(readbytes())
 while True:
-    if ser.in_waiting:
-        print(ser.in_waiting)
-        print("Recieved back: ", ser.readline())
+    x = time.time()
+    frame = wav.readframes(framerate)
+    if frame == b'':
+        break
+    # print(frame)
+    # frame = unpack("<B", frame)[0]
+
+    ser.write(frame)
+    T = 1 - (time.time() - x)
+    time.sleep(T)
+    print("time: ", time.time() - x)
+    # ser.write(pack("<cc", frame, b'\n'))
+    # ser.write(frame)
+    # readbytes()
